d2d185ceb7e0cc24419fd9ded67d5037d4c57e2c
Velvet Rock <129342804+velvetrock9@users.noreply.github.com>
2023-11-22 17:25:42 +0100

    Delete elpa directory
    delete unnecessary elpa directory

--- elpa/orangey-bits-theme-20220822.324/orangey-bits-theme-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "orangey-bits-theme" "20220822.324" "A Theme with smashing orangey bits"
-  '((autothemer "0.2")
-    (emacs "27.1"))
-  :commit "533856d399cb4098300bcaf4a2d20920395746f8" :url "http://github.com/emacsfodder/emacs-theme-orangey-bits")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
